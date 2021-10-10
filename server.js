6216f0276e98c83d739631a740fcfe86359ccc5f
Utsav Mishra <utsavmishra1411@gmail.com>
2021-10-10 13:59:10 +0530

    deploying apis

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const http = require('http')
 const port = process.env.port || 3000
-const host = process.env.YOUR_HOST || '0.0.0.0';
 const app = require('./app')
 const server = http.createServer(app)
-server.listen(port, host)
+server.listen(port, '0.0.0.0')
