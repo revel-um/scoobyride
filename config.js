5bdaa6d1d5f528543d146ccd0d6c70d7ccdfcb7a
Utsav Mishra <utsavmishra1411@gmail.com>
2022-01-29 02:36:45 +0530

    aws apis

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     serviceID: "VA8aa1b41f6cff5ea9719302304e91a3b0",
     accountSID: "ACb81743c8771c3bc96d66025b37e38c4d",
-    authToken: "0454275f05c9540110b9d53ed08b0e5e"
+    authToken: "be0dd7358bbbc14270dbf55eff99d0c0"
 }
