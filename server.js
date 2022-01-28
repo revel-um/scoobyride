303d415beed0d3a1a581b47b1fd3b6e3c890e637
Utsav Mishra <utsavmishra1411@gmail.com>
2022-01-28 19:45:02 +0530

    aws apis

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-const http = require('http')
-console.log('http worked')
+// const http = require('http')
+// console.log('http worked')
+const express = require('express')
 const port = 3000
-const app = require('./app')
-console.log(port)
-const server = http.createServer(app)
-server.listen(port)
+// const app = require('./app')
+const app = express();
+app.listen(port, ()=>{
+    console.log('listing to calls');
+})
+// console.log(port)
+// const server = http.createServer(app)
+// server.listen(port)
