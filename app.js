0a9c76195ddfb208a09972b0ebf0b2963ace97bd
Utsav Mishra <utsavmishra1411@gmail.com>
2022-01-28 20:10:55 +0530

    aws apis

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express')
-// const store = require('./routes/store')
-// const product = require('./routes/product')
+const store = require('./routes/store')
+const product = require('./routes/product')
 const app = express()
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-// const verification = require('./routes/verification')
-// const user = require('./routes/user')
-// const order = require('./routes/order')
+const verification = require('./routes/verification')
+const user = require('./routes/user')
+const order = require('./routes/order')
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
@@ -16,11 +16,11 @@ const connectDB = async () => { await mongoose.connect('mongodb+srv://utsav:utsa
 connectDB();
 
 
-// app.use('/verification', verification)
-// app.use('/stores', store)
-// app.use('/products', product)
-// app.use('/users', user)
-// app.use('/orders', order)
+app.use('/verification', verification)
+app.use('/stores', store)
+app.use('/products', product)
+app.use('/users', user)
+app.use('/orders', order)
 
 
 app.use('/', (req, res, next) => {
