795a438cced56f6d70a3ff4bcc66436613974500
Utsav Mishra <utsavmishra1411@gmail.com>
2022-01-31 03:27:40 +0530

    aws apis

--- schemas/productSchema.js
+++ schemas/productSchema.js
@@ -4,19 +4,17 @@ const productSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     model: { type: String, required: true },
     licencePlate: { type: String },
-    rentPerHour: { type: String, required: true },
+    rentPerHour: { type: String },
     rentPerDay: { type: String, required: true },
     productImages: { type: [String], default: [] },
     store: { type: mongoose.Schema.Types.ObjectId, ref: 'Store', require: true },
     criteria: { type: String, required: true },
-    isVeg: { type: Boolean, default: true },
     details: String,
     bullets: String,
     size: String,
-    price: Number,
     discount: Number,
     isDiscountPercent: { type: Boolean, default: true },
-    outOfStock: { type: Boolean, default: false }
+    booked: { type: Boolean, default: false }
 })
 
 module.exports = mongoose.model('Product', productSchema)
