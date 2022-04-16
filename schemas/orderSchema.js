bbb0ecf5f8cffffdc49832898152ce8ea354846e
Utsav Mishra <utsavmishra1411@gmail.com>
2022-04-16 20:47:23 +0530

    Aws apis

--- schemas/orderSchema.js
+++ schemas/orderSchema.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 //State --> {0: 'Cart', 1: 'Order Confirmed', 2: 'Order Complete', 3: 'Favourites'}
 const orderSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    items: [{ product: { type: mongoose.Schema.Types.ObjectId, ref: "Product" }, state: Number, store:{type: mongoose.Schema.Types.ObjectId, ref: "Product"} }],
+    items: [{ product: { type: mongoose.Schema.Types.ObjectId, ref: "Product" }, state: Number, store:{type: mongoose.Schema.Types.ObjectId, ref: "Store"} }],
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, unique: true },
 });
 
